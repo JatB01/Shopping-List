8994aa38c2fa2a5af4a8a234c2d674558ca465e0
JatB01 <38192190+JatB01@users.noreply.github.com>
2018-08-11 14:21:58 +0100

    adding files to github

---
+++ script.js
@@ -0,0 +1,69 @@
+//Declare variables
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var ClearAll = document.getElementById("clear");
+var listItems = document.getElementsByTagName("li");
+
+//returns length of list
+function listLength() {
+  return listItems.length;
+}
+
+//returns length of user input
+function inputLength() {
+  return input.value.length;
+}
+
+//creates a new list element & clears the search bar
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+  var btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("x"));
+  li.appendChild(btn);
+  btn.onclick = removeParent;
+}
+
+//adds element once button clicked
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+// creates element once they press enter
+function addListAfterKeyPress(event) {
+  if (inputLength() > 0 && event.keyCode == 13) {
+    createListElement();
+  }
+}
+//creates button with child text delete.
+function deleteButton() {
+  var btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("x"));
+  //Adds to existing list
+  listItems[i].appendChild(btn);
+  btn.onclick = removeParent;
+}
+//runs to add delete buttons to existing items
+for (i = 0; i < listLength(); i++) {
+  deleteButton();
+}
+
+//removes parent node
+function removeParent(evt) {
+  evt.target.parentNode.remove();
+}
+
+ClearAll.onclick = function(evt) {
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+};
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeyPress);
