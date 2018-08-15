64f68ffd8274da597f545d11f0c37e3b388e9fb2
JatB01 <38192190+JatB01@users.noreply.github.com>
2018-08-16 00:42:25 +0100

    test2

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ for (i = 0; i < listLength(); i++) {
 }
 
 //removes parent node. First checks if parent node is <li> and if so, removes parent. Otherwise it will be <s> so removes grandparent <li>
-$(document).on("click", "", function() {
+$(document).on("click", "#del", function() {
   // console.log($(this));
   if (
     $(this)
